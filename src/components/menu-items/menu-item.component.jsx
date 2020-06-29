11e33872458a0cac4dafaa4e9b7b911ca4d2fc19
Douglas Fleischut <df@douglass-imac.lan>
2020-06-29 11:45:10 -0700

    menu items finished, applied styles

---
+++ src/components/menu-items/menu-item.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => {
+    return (
+
+        <div 
+        className={`${size} menu-item`}
+        >
+        <div style={{
+            backgroundImage: `url(${imageUrl})`
+            }}
+            className="background-image"
+            />   
+        <div className="content">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span className="subtitle">shop now</span>    
+        </div>
+        </div>
+    )
+
+}
+
+export default MenuItem;
