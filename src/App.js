0da6fdb02e413fdd7ade260ffa05267f967ddf66
Douglas Fleischut <df@douglass-imac.lan>
2020-06-30 11:12:20 -0700

    update shop page oops did it on master branch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 export const HatsPage = () => (
   <div>
@@ -16,7 +17,7 @@ function App() {
       {/* <HomePage /> */}
       <Switch>
       <Route exact path='/' component={HomePage} /> 
-      <Route exact path='/shop/hats' component={HatsPage} /> 
+      <Route exact path='/shop' component={ShopPage} /> 
       </Switch>
     </div>
   );
