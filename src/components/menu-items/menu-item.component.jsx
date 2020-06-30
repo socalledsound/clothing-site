274884313e7f8cfbc8ecbe7cbd147bd51dd9134a
Douglas Fleischut <df@douglass-imac.lan>
2020-06-30 09:57:26 -0700

    basic routing using withRouter

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
     return (
 
         <div 
+        onClick = {()=> history.push(`${match.url}${linkUrl}`)}
         className={`${size} menu-item`}
         >
         <div style={{
@@ -21,4 +23,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
 
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
