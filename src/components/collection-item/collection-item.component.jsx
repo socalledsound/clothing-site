f0fa7158bcdd6a1a16fd34c61fffa1d7adcfb525
Douglas Fleischut <df@douglass-imac.lan>
2020-07-04 15:10:43 -0700

    updated cart reducer with find utility

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl}) => (
-    <div className='collection-item'>
+const CollectionItem = ({ item, addItem }) => {
+    
+    const {name, price, imageUrl} = item;
+    return (
+        <div className='collection-item'>
         <div 
         className="image"
         style={{
@@ -14,8 +19,15 @@ const CollectionItem = ({ id, name, price, imageUrl}) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
-        <CustomButton inverted>add to cart</CustomButton>
+        <CustomButton inverted onClick={()=>addItem(item)}>add to cart</CustomButton>
     </div>
-)
+    )
+}
 
-export default CollectionItem;
+
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
