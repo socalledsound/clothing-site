bdddd1c297794cc1bdd5f6feb5bccde7ec3a94eb
Douglas Fleischut <df@douglass-imac.lan>
2020-07-04 16:06:05 -0700

    updated cart with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import './cart-dropdown.styles.scss';
 
 const CartDropDown = ({ cartItems }) => (
@@ -19,8 +20,8 @@ const CartDropDown = ({ cartItems }) => (
 )
 
 
-const mapStateToProps = ({cart: { cartItems }}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropDown)
